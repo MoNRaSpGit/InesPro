6172ed4a8e7563c1ce5886f40d2ed82b1edc4a39
Ramon <ju4nrsuarez@gmail.com>
2024-12-09 06:56:10 -0300

    f5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // src/index.js
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS globalmente
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client'; // Importamos desde 'react-dom/client' en lugar de 'react-dom'
+import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Componente principal que maneja redirección en recarga
+function Main() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!window.location.pathname || window.location.pathname !== '/') {
+      navigate('/'); // Redirige al login si se recarga en otra ruta
+    }
+  }, [navigate]);
+
+  return <App />;
+}
 
 // Obtén el contenedor raíz del DOM
 const container = document.getElementById('root');
@@ -12,7 +26,9 @@ const root = ReactDOM.createRoot(container); // Crea el root con createRoot
 // Renderiza la aplicación usando el nuevo método
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Main />
+    </Router>
   </React.StrictMode>
 );
 
